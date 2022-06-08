e8ed69bf7c4ff17f1abf7e32c609093dc5b4a3de
wagner <wagncarv@outlook.com>
2022-06-08 19:34:59 -0300

    armazenamento de tentativas

--- lib/guess.ex
+++ lib/guess.ex
@@ -54,28 +54,32 @@ defmodule Guess do
   def play(picked_num) do
     IO.gets("I have my number. What is your guess? ")
     |> parse_input()
-    |> guess(picked_num, 1)
+    |> guess(picked_num, 1, [])
   end
 
-  def guess(usr_guess, picked_num, count) when usr_guess > picked_num do
-    IO.gets("Too high. Guess again: ")
+  def guess(usr_guess, picked_num, count, guesses_track) when usr_guess > picked_num do
+    number = IO.gets("Too high. Guess again: ")
     |> parse_input()
-    |> guess(picked_num, count + 1)
+    guessed_number_message(number, guesses_track)
+    guesses_track = guesses_track ++ [number]
+    guess(number, picked_num, count + 1, guesses_track)
   end
 
-  def guess(usr_guess, picked_num, count) when usr_guess < picked_num do
-    IO.gets("Too low. Guess again: ")
+  def guess(usr_guess, picked_num, count, guesses_track) when usr_guess < picked_num do
+    number = IO.gets("Too low. Guess again: ")
     |> parse_input()
-    |> guess(picked_num, count + 1)
+    guessed_number_message(number, guesses_track)
+    guesses_track = guesses_track ++ [number]
+    guess(number, picked_num, count + 1, guesses_track)
   end
 
-  def guess(_usr_guess, _picked_num, count) do
+  def guess(_usr_guess, _picked_num, count, guesses_track) do
     IO.puts("You got it #{count} #{if count > 1 do "guesses" else "guess" end} !")
-    show_score(count)
+    show_score(count, guesses_track)
   end
-  def show_score(guesses) when guesses > 6, do: "Better luck next time !"
-  def show_score(guesses) do
-    %{
+  def show_score(guesses, guesses_track) when guesses > 6, do: %{message: "Better luck next time !", tried_numbers: guesses_track}
+  def show_score(guesses, guesses_track) do
+    message = %{
       (1..1) => "You're a mind reader !",
       (2..4) => "Very impressive",
       (3..6) => "You can do better than that"
@@ -84,5 +88,18 @@ defmodule Guess do
       Enum.member?(range, guesses)
     end)
     |> elem(1)
+    %{message: message, tried_numbers: guesses_track}
+  end
+
+  defp guessed_number_message(number, guesses_track) do
+    case number in guesses_track do
+      false -> nil
+      true -> IO.puts "Number #{number} already picked !"
+    end
+  end
+
+  def parse_numbers(guesses_track) do
+    guesses_track
+    # Integer.parse()
   end
 end
