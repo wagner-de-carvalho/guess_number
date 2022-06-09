25e5482cb123260c5612796f389af572b53acbd1
wagner <wagncarv@outlook.com>
2022-06-09 09:29:44 -0300

    armazenar primeiro palpite

--- lib/guess.ex
+++ lib/guess.ex
@@ -52,22 +52,27 @@ defmodule Guess do
   end
 
   def play(picked_num) do
-    IO.gets("I have my number. What is your guess? ")
+    number = IO.gets("I have my number. What is your guess? ")
     |> parse_input()
-    |> guess(picked_num, 1, [])
+
+    guess(number, picked_num, 1, [number])
   end
 
   def guess(usr_guess, picked_num, count, guesses_track) when usr_guess > picked_num do
-    number = IO.gets("Too high. Guess again: ")
-    |> parse_input()
+    number =
+      IO.gets("Too high. Guess again: ")
+      |> parse_input()
+
     guessed_number_message(number, guesses_track)
     guesses_track = guesses_track ++ [number]
     guess(number, picked_num, count + 1, guesses_track)
   end
 
   def guess(usr_guess, picked_num, count, guesses_track) when usr_guess < picked_num do
-    number = IO.gets("Too low. Guess again: ")
-    |> parse_input()
+    number =
+      IO.gets("Too low. Guess again: ")
+      |> parse_input()
+
     guessed_number_message(number, guesses_track)
     guesses_track = guesses_track ++ [number]
     guess(number, picked_num, count + 1, guesses_track)
@@ -77,24 +82,29 @@ defmodule Guess do
     IO.puts("You got it #{count} #{if count > 1 do "guesses" else "guess" end} !")
     show_score(count, guesses_track)
   end
-  def show_score(guesses, guesses_track) when guesses > 6, do: %{message: "Better luck next time !", tried_numbers: guesses_track}
+
+  def show_score(guesses, guesses_track) when guesses > 6,
+    do: %{message: "Better luck next time !", tried_numbers: guesses_track}
+
   def show_score(guesses, guesses_track) do
-    message = %{
-      (1..1) => "You're a mind reader !",
-      (2..4) => "Very impressive",
-      (3..6) => "You can do better than that"
-    }
-    |> Enum.find(fn {range, _} ->
-      Enum.member?(range, guesses)
-    end)
-    |> elem(1)
+    message =
+      %{
+        (1..1) => "You're a mind reader !",
+        (2..4) => "Very impressive",
+        (3..6) => "You can do better than that"
+      }
+      |> Enum.find(fn {range, _} ->
+        Enum.member?(range, guesses)
+      end)
+      |> elem(1)
+
     %{message: message, tried_numbers: guesses_track}
   end
 
   defp guessed_number_message(number, guesses_track) do
     case number in guesses_track do
       false -> nil
-      true -> IO.puts "Number #{number} already picked !"
+      true -> IO.puts("Number #{number} already picked !")
     end
   end
 
